cd94101e085fb02e1e332d60bf458b1fedd5a6ad
Eligijus Bujokas <Eligijus.Bujokas@Euromonitor.com>
2018-12-18 14:59:41 +0200

    added the first step for xgboost

--- master.py
+++ master.py
@@ -13,12 +13,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
+import xgboost as xgb
 
 from preprocess import clean
 
 ## Hyper parameters
 
-test_share = 0
+test_share = 0.3
 
 ## Reading data
 
@@ -58,6 +59,16 @@ dtm_train = vect.fit_transform(X_train)
 
 ## Creating a model
 
+### boosting 
+
+xg_log = xgb.XGBClassifier(objective ='reg:logistic', 
+                           colsample_bytree = 0.3, 
+                          learning_rate = 0.1,
+                max_depth = 5, alpha = 10, n_estimators = 20)
+fitted_xgboost = xg_log.fit(dtm_train, Y_train)
+
+### Logistic
+
 model = LogisticRegression(solver = 'newton-cg')
 fitted_model = model.fit(dtm_train, Y_train)
 
@@ -83,6 +94,7 @@ dtm_test = vect.fit_transform(X_test)
 ### Prediction
 
 y_hat = fitted_model.predict(dtm_test)
+y_hat_boost = fitted_xgboost.predict(dtm_test)
 
 if(test_share is not 0):
     
@@ -90,7 +102,12 @@ if(test_share is not 0):
     
     y_actual = Y_test.tolist()
     y_hat = y_hat.tolist()
+    print('Logistic regression:')
     print(f1_score(y_actual, y_hat))
+    
+    y_hat_boost = y_hat_boost.tolist()
+    print('Xgboost:')
+    print(f1_score(y_actual, y_hat_boost))
 
 if(test_share == 0):
     
